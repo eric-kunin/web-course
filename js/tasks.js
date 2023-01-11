792aa45a2ba41b79299892f78ff375446c4def2e
eric-kunin <eric.konin29@gmail.com>
2023-01-11 18:36:50 +0200

    initial commit

---
+++ js/tasks.js
@@ -0,0 +1,9 @@
+// const main = document.querySelector('.main');
+// const img = document.createElement('img');
+// img.classList.add('myFormbg','center-image');
+// img.src = "img/formbg.jpg";
+// img.alt = "formbg";
+
+// for(var i = 0; i < 4; i++) {
+//   main.appendChild(img.cloneNode(true));
+// }
