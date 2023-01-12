4c88769b951e60648c660516caaff14cef0df079
eric-kunin <eric.konin29@gmail.com>
2023-01-12 02:53:38 +0200

    added js

--- js/tasks.js
+++ js/tasks.js
@@ -4,5 +4,17 @@ const addNewMission = () => {
     newMission.missionData = document.getElementById("missionData").value;
     newMission.missionDate = document.getElementById("missionDate").value;
     missionList.push(newMission);
+
+    // Create an image element with the user's text
+    var newImg = document.createElement("img");
+    newImg.src = "img/notebg.png"; // you should use the URL of the image you want to display
+    newImg.style.cssText = "width:200px; margin-top:20px"  // you can customize the style of the image
+
+    // Append the image to the HTML document
+    var footer = document.getElementById("footer");
+    footer.appendChild(newImg);
+
+    // reset the form
     document.getElementById("myForm").reset();
-  };
+};
+
