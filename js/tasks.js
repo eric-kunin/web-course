881df161f8ff263067980ffb6190ac3fac1e112d
eric-kunin <eric.konin29@gmail.com>
2023-01-12 19:57:42 +0200

    added time in html

--- js/tasks.js
+++ js/tasks.js
@@ -1,18 +1,39 @@
 var missionList = [];
 const addNewMission = () => {
     const newMission = new Object();
-    newMission.missionData = document.getElementById("missionData").value;
+    missionData = document.getElementById("missionData").value;
     newMission.missionDate = document.getElementById("missionDate").value;
+    newMission.missionDate = missionData;
     missionList.push(newMission);
 
     // Create an image element with the user's text
-    var newImg = document.createElement("img");
-    newImg.src = "img/notebg.png"; // you should use the URL of the image you want to display
-    newImg.style.cssText = "width:200px; margin-top:20px"  // you can customize the style of the image
+    // Create a container div element
+    var container = document.createElement("div");
+    container.style.cssText = "position:relative; width:200px; height:200px;";
 
-    // Append the image to the HTML document
+    // Create an image element
+    var newImg = document.createElement("img");
+    newImg.src = "img/notebg.png";
+    newImg.style.cssText = "width:100%; height:100%;";
+
+    // Create a text div element
+    var text = document.createElement("div");
+    text.innerText = missionData;
+    // text.style.cssText = "position:absolute; top:50%; left:50%; transform:translate(-50%,-50%); color:black;overflow: auto;";
+    // text.style.cssText = "position:absolute; top:50%; left:50%; transform:translate(-50%,-50%); color:black;overflow:auto; width:50%; height:50%;";
+    // ok code
+    // text.style.cssText = "position:absolute; top:50%; left:50%; transform:translate(-50%,-50%); color:black;overflow-x:scroll;overflow-y:scroll;width:50%; height:50%;";
+    text.style.cssText = "position:absolute; left:33px; top:35px; width:68%; height:55%; color:black;overflow-x:scroll;overflow-y:scroll;";
+
+
+    // Append the elements to the container
+    container.appendChild(newImg);
+    container.appendChild(text);
+
+    // Append the container to the HTML document
     var footer = document.getElementById("footer");
-    footer.appendChild(newImg);
+    footer.appendChild(container);
+
 
     // reset the form
     document.getElementById("myForm").reset();
