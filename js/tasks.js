08fd160f45a4e5e3dfe9d45a990e4e97224c2265
eric-kunin <eric.konin29@gmail.com>
2023-01-13 19:09:18 +0200

    added date

--- js/tasks.js
+++ js/tasks.js
@@ -2,9 +2,8 @@ var missionList = [];
 const addNewMission = () => {
     const newMission = new Object();
     missionData = document.getElementById("missionData").value;
-    newMission.missionDate = document.getElementById("missionDate").value;
+    var missionDate = new Date(document.getElementById("missionDate").value);
     newMission.missionDate = missionData;
-    newMission.missionTime = document.getElementById("missionTime").value;
     missionList.push(newMission);
 
     // Create an image element with the user's text
@@ -20,11 +19,18 @@ const addNewMission = () => {
     // Create a textarea element
     var text = document.createElement("textarea");
     text.innerText = missionData;
-    text.style.cssText = "position:absolute; left:7px; top:35px; width:82%; height:55%; color:black;overflow-x:scroll;overflow-y:scroll;resize:none;";
+    text.style.cssText = "position:absolute; left:7px; top:34px; width:82%; height:58%; color:black;overflow-x:scroll;overflow-y:scroll;resize:none;";
 
+    // Create a date element
+    var date = document.createElement("div");
+    // date.innerText = missionDate.toLocaleDateString("he-IL", {day: "2-digit", month: "2-digit", year: "numeric"});
+    date.innerText = missionDate.getDate() + '/' + (missionDate.getMonth()+1) + '/' + missionDate.getFullYear();
+
+    date.style.cssText = "position:absolute; left:7px; top:77%; width:82%; height:5%; color:black;";
     // Append the elements to the container
     container.appendChild(newImg);
     container.appendChild(text);
+    container.appendChild(date);
 
     // Append the container to the HTML document
     var footer = document.getElementById("footer");
