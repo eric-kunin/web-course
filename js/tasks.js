19fa104ced365b7dbac3d0d21e4e64643a25aac6
eric-kunin <eric.konin29@gmail.com>
2023-01-13 19:27:57 +0200

    added time

--- js/tasks.js
+++ js/tasks.js
@@ -1,9 +1,13 @@
 var missionList = [];
 const addNewMission = () => {
     const newMission = new Object();
+    // data
     missionData = document.getElementById("missionData").value;
+    // date
     var missionDate = new Date(document.getElementById("missionDate").value);
     newMission.missionDate = missionData;
+    // time
+    missionTime = document.getElementById("missionTime").value;
     missionList.push(newMission);
 
     // Create an image element with the user's text
@@ -19,19 +23,25 @@ const addNewMission = () => {
     // Create a textarea element
     var text = document.createElement("textarea");
     text.innerText = missionData;
-    text.style.cssText = "position:absolute; left:7px; top:34px; width:82%; height:58%; color:black;overflow-x:scroll;overflow-y:scroll;resize:none;";
+    text.style.cssText = "position:absolute; left:7px; top:28px; width:82%; height:58%; color:black;overflow-x:scroll;overflow-y:scroll;resize:none;";
 
     // Create a date element
     var date = document.createElement("div");
     // date.innerText = missionDate.toLocaleDateString("he-IL", {day: "2-digit", month: "2-digit", year: "numeric"});
     date.innerText = missionDate.getDate() + '/' + (missionDate.getMonth()+1) + '/' + missionDate.getFullYear();
-
     date.style.cssText = "position:absolute; left:7px; top:77%; width:82%; height:5%; color:black;";
+
+    // Create a time element
+    var time = document.createElement("div");
+    time.innerText = missionTime;
+    time.style.cssText = "position:absolute; left:7px; top:85%; width:82%; height:5%; color:black;";
     // Append the elements to the container
     container.appendChild(newImg);
     container.appendChild(text);
     container.appendChild(date);
+    container.appendChild(time);
 
+    // Append the container
     // Append the container to the HTML document
     var footer = document.getElementById("footer");
     footer.appendChild(container);
