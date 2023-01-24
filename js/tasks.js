1503807daf0d5e73f69a7521b9ed37b6ee1f90b6
eric-kunin <eric.konin29@gmail.com>
2023-01-24 04:55:11 +0200

    added textarea

--- js/tasks.js
+++ js/tasks.js
@@ -4,9 +4,9 @@ const addNewMission = () => {
     // data of note/mission
     newMission.missionData = document.getElementById("missionData").value;
     // date of note/mission
-    newMission.missionData = document.getElementById("missionDate").value;
+    newMission.missionDate = document.getElementById("missionDate").value;
     // time of note/mission
-    newMission.missionData = document.getElementById("missionTime").value;
+    newMission.missionTime = document.getElementById("missionTime").value;
     missionList.push(newMission);
     insertData();
   };
@@ -17,10 +17,11 @@ const addNewMission = () => {
     missionList.map((item) => {
       myHTML += `
       <div class="col-lg-3 col-md-3 col-sm-6 my-2 my-class">
-  <img src="img/notebg.png" alt="notebg"; style="width:75%; height:95%;"/>
+  <img src="img/notebg.png" alt="notebg"; style="width:75%; height:75%;"/>
+  <textarea id="textarea" style="position: relative;top: -63%;left:5%;width:55%; height:43%; color:black;overflow-x:scroll;overflow-y:scroll;resize:none;">${item.missionData}</textarea>
 </div>
       `;
     });
     tableData.innerHTML = myHTML;
   };
-  
+// "position:absolute; left:14px; top:28px; width:73%; height:61%; color:black;overflow-x:scroll;overflow-y:scroll;resize:none;"
