2c3f840e3acd38a5a383d97e7ddcd9908d403550
eric-kunin <eric.konin29@gmail.com>
2023-01-27 20:25:15 +0200

    added X

--- js/note.js
+++ js/note.js
@@ -1,5 +1,9 @@
-//  If localStorage.getItem("noteList") returns null, then an empty array is assigned to missionList
-var noteList = JSON.parse(localStorage.getItem("noteList")) || []; 
+let noteList = JSON.parse(localStorage.getItem("noteList")) || []; 
+
+window.onload = function() {
+  createNotes();
+}
+
 const handleForm = () => {
     var newNote = new Object();
     // data of note/mission
@@ -25,7 +29,7 @@ const handleForm = () => {
       myHTML += `
       <div id="myDiv-${index}" class="col-lg-3 col-md-3 col-sm-6 my-2 my-class" style="position:relative;opacity:0; animation: fadein 0.5s; animation-fill-mode: forwards;">
   <img src="img/notebg.png" id="img" alt="notebg"; style="width:75%; height:85%;"/>
-  <button class="btn" onclick="removeNote(${index})"><img src="img/close.png" width="20px"/></button>
+  <button class="btn" onclick="removeNote(${index})"><img src="img/close.png" width="35px";/></button>
   <textarea id="myData" style="position:absolute;top:3%;left:5%;width:55%; height:43%; color:black;overflow-x:scroll;overflow-y:scroll;resize:none;">${item.noteData}</textarea>
   <span id="myDate" style="position:absolute;color:black;">${pretifyDate(item.noteDate)}</span>
   <span id="myTime" style="position:absolute;color:black;">${item.noteTime}</span>
@@ -34,6 +38,7 @@ const handleForm = () => {
     });
     tableData.innerHTML = myHTML;
   };
+
 
 const pretifyDate = (noteTime) => {
     const myNewDate = noteTime.split("-");
@@ -47,4 +52,4 @@ const  removeNote=(index) =>{
   createNotes()
 }
 
-// sfsdfsdfsd
+// <button class="btn" onclick="removeNote(${index})"><span class="glyphicon glyphicon-remove"></span></button>
