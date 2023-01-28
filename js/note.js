25882668b8618b6491291fa177e70ac2ccd81930
eric-kunin <eric.konin29@gmail.com>
2023-01-28 13:19:12 +0200

    made smaller input

--- js/note.js
+++ js/note.js
@@ -28,8 +28,8 @@ const handleForm = () => {
     noteList.map((item,index) => {
       myHTML += `
       <div id="myDiv-${index}" class="col-lg-3 col-md-3 col-sm-6 my-2 my-class" style="position: relative;">
-  <img src="img/notebg.png" id="img" alt="notebg" style="width:75%; height:85%;"/>
-  <button class="btn" onclick="removeNote(${index})" style="position:absolute;top:5%;left:45%;color:black;"><img src="img/close2.png" width="25"/></button>
+  <img src="https://i.postimg.cc/NGPxxkkb/notebg.png" id="img" alt="notebg" style="width:75%; height:85%;"/>
+  <button class="btn" onclick="removeNote(${index})" style="position:absolute;top:5%;left:45%;color:black;"><img src="https://i.postimg.cc/J0YFjy1n/close2.png" width="25"/></button>
   <p style="position:absolute;top:15%;left:11%;color:black;max-width:150px;
   max-height: 120px;
   overflow-x: auto;
