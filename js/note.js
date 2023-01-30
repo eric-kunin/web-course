c77ada541bf3e0247984efbf0b97d4c835882f96
eric-kunin <eric.konin29@gmail.com>
2023-01-30 23:04:59 +0200

    added original files

--- js/note.js
+++ js/note.js
@@ -1,52 +1,59 @@
+// parsing note data from local storage or creating an empty array if there's no data stored
 let noteList = JSON.parse(localStorage.getItem("noteList")) || []; 
 
+// on page load, create note elements
 window.onload = function() {
   createNotes();
 }
 
-// It will only work once 
+// flag to track whether animation has run
 let animationRun = false;
-
+// on window load, apply fadeIn animation to all elements with class "my-class"
 window.addEventListener("load", function() {
   if (!animationRun) {
     // select all elements with the class "my-class"
     const elements = document.querySelectorAll(".my-class");
-
     // apply the "fadeIn" animation to each element
     elements.forEach(element => {
       element.style.animation = "fadeIn 1s";
       element.style.animationFillMode = "forwards";
     });
-    
+    // set flag to true to indicate that animation has run
     animationRun = true;
   }
 });
 
-
+// function to handle form submission
 const handleForm = () => {
+    // creating a new note object
     var newNote = new Object();
-    // data of note/mission
+    // storing note data from form input
     newNote.noteData = document.getElementById("noteData").value;
-    // date of note/mission
+    // storing note date from form input
     newNote.noteDate = document.getElementById("noteDate").value;
-    // time of note/mission
+    // storing note time from form input
     newNote.noteTime = document.getElementById("noteTime").value;
-    // adding
+    // adding the new note object to the note list
     noteList.push(newNote);
-    // reset
+    // resetting the form
     document.getElementById('myForm').reset()
-    // Save the mission data to LocalStorage
+    // saving the note list to local storage
     localStorage.setItem("noteList", JSON.stringify(noteList));
-    // func
+    // creating note elements
     createNotes();
+    // fading in the last added note
     fadeNote();
   };
   
-  const createNotes = () => {
-    const tableData = document.getElementById("footer");
-    var myHTML = "";
-    noteList.map((item,index) => {
-      myHTML += `
+// function to create note elements on the page
+const createNotes = () => {
+  // get reference to tableData element with id "footer"
+  const tableData = document.getElementById("footer");
+  // variable to store the HTML string that will be added to the tableData element
+  var myHTML = "";
+  // loop through the noteList array and generate HTML for each note
+  noteList.map((item,index) => {
+    myHTML += `
       <div id="noteNumber-${index}" class="col-lg-3 col-md-3 col-sm-6 my-2 my-class image-container" style="position: relative;">
         <button class="btn deleteNote" id="deleteNote" onclick="removeNote(${index})" style="position:absolute;top:5%;left:66%;color:black;margin: auto;">
           <i class="fa-solid fa-xmark" style="font-size:205%;"></i>
@@ -59,28 +66,39 @@ const handleForm = () => {
         <span id="noteDate" style="position:absolute;top:77%;left:14%;color:black;font-weight:bold;">${pretifyDate(item.noteDate)}</span>
         <span id="noteTime" style="position:absolute;top:85%;left:14%;color:black;font-weight:bold;">${item.noteTime}</span>
       </div>
-      `;
-    });
-    tableData.innerHTML = myHTML;
-    // end
-  };
+    `;
+  });
+  // add the generated HTML to the tableData element
+  tableData.innerHTML = myHTML;
+};
+
+// Function to prettify the date
 const pretifyDate = (noteDate) => {
-    const myNewDate = noteDate.split("-");
-    return `${myNewDate[2]}/${myNewDate[1]}/${myNewDate[0]}`;
-  };
-// remove the localstorage
-// localStorage.removeItem("noteList");
+  // Split the date string by "-" character and store it in an array
+  const myNewDate = noteDate.split("-");
+  // Return the formatted date string in "dd/mm/yyyy" format
+  return `${myNewDate[2]}/${myNewDate[1]}/${myNewDate[0]}`;
+};
+
+// Function to remove a note
 const  removeNote=(index) =>{
-  noteList.splice(index,1);
-  localStorage.setItem('noteList', JSON.stringify(noteList));
-  createNotes();
+// Remove the note at the specified index
+noteList.splice(index,1);
+// Update the note list in local storage
+localStorage.setItem('noteList', JSON.stringify(noteList));
+// Call the function to re-render the notes
+createNotes();
 }
 
+// Function to fade the last note added
 const fadeNote =()=>{
-  var lastIndex = noteList.length-1;
-  var elDiv = document.getElementById(`noteNumber-${lastIndex}`);
-  elDiv.classList.add('fade');
+// Get the index of the last note in the note list
+var lastIndex = noteList.length-1;
+// Get the DOM element for the last note
+var elDiv = document.getElementById(`noteNumber-${lastIndex}`);
+// Add the fade class to the last note DOM element
+elDiv.classList.add('fade');
 }
 
-// <button class="btn" onclick="removeNote(${index})"><span class="glyphicon glyphicon-remove"></span></button>
+
 
